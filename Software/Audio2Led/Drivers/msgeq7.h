46d6734bfb0cea5c463464af10bbb6c9e781626b
jfritz022 <jfritz022@gmail.com>
2017-03-06 19:58:20 -0600

    Initial Commit

---
+++ Software/Audio2Led/Drivers/msgeq7.h
@@ -0,0 +1,15 @@
+/** @file module.h
+*
+* @brief A description of the module's purpose.
+*
+* @par
+* COPYRIGHT NOTICE: (c) 2017 John Fritz
+* All rights reserved
+*/
+
+#ifndef _MODULE_H
+#define _MODULE_H
+
+void adc_read(uint8_t adc_mux, uint16_t *adc_val, uint16_t adc_val_len);
+
+#endif /* _MODULE_H */
---
+++ Software/Audio2Led/main.c
@@ -0,0 +1,30 @@
+/*
+ * Audio2Led
+ *
+ * Created: 3/6/2017 7:22:31 PM
+ * Author : John
+ */ 
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+void system_init(void);
+
+
+int main(void)
+{
+    system_init();
+
+    while (1) 
+    {
+    }
+}
+
+void system_init(void)
+{
+    
+    
+}
+
